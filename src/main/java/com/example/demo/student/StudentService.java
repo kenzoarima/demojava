67a57f28855734de69cc62422f7ee86ec2e953b4
Kenzo <suprabluesil@gmail.com>
2022-08-07 21:49:20 +0800

    initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    private final StudentRepository repository;
+
+    @Autowired
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+    
+    public List<Student> getStudents() {
+        return repository.findAll();
+	}
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = repository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        repository.save(student);
+        //System.out.println(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = repository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        repository.deleteById(studentId);
+    }
+}
